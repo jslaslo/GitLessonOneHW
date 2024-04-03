6fddafa8e56a897df7ee3529026837b0f8fd2f48
dergousov.vladislav <125125092+jslaslo@users.noreply.github.com>
2024-04-03 08:11:04 +0200

    added my project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitLessonOneHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
